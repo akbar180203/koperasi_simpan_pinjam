7301ffdd8d1fdae9b282816ea47b857ff2aea32e
akbar180203 <akbarfirmansyah64419@gmailcom>
2023-10-06 07:01:40 +0700

    login

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <?php
 // cek apakah tombol submit sudah ditekan atau belum
-if( isset($_POST["submit"]))  {
-  // cek username & password
-  if( $_POST["username"] == "admin" && $_POST["password"] == "12345") {
-      // jika benar, redirect ke halaman admin
-      header("Location: ./admin.html");
-      exit;
-    }else{
-    // jika salah, tampilkan kesalahan
-    echo $_POST ["username/password salah"];
+if(isset($_POST["submit"]))  {
+    // cek username & password
+    if($_POST["username"] == "admin" && $_POST["password"] == "12345") {
+        // jika benar, redirect ke halaman admin
+        header("Location: ./admin.html");
+        exit;
+    } else {
+        // jika salah, tampilkan pesan pop up
+        echo "<script>alert('Login gagal. Periksa kembali username dan password Anda.');
+        window.location.href='./UI/login.html';</script>";
     }
 }
 ?>
